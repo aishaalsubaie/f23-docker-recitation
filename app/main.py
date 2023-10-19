0c727c7eec2e9993da3e811bc39fb7387c07b9d4
aishaalsubaie <asubaie@andrew.cmu.edu>
2023-10-19 13:37:26 +0300

    docker files

--- app/main.py
+++ app/main.py
@@ -3,16 +3,14 @@ import requests
 
 app = FastAPI()
 
+# Recitation hours dictionary
+RECITATION_HOURS = {"a": "09:00~09:50", "b": "10:00~10:50", "c": "11:00~11:50", "d": "12:00~12:50"}
+# Microservice link
+MICROSERVICE_LINK = "https://whos-my-ta.fly.dev/section_id/"
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
-
-RECITATION_HOURS = {"a": "09:00~09:50", "b": "10:00~10:50",
-                    "c": "11:00~11:50", "d": "12:00~12:50"}
-MICROSERVICE_LINK = "https://whos-my-ta.fly.dev/section_id/"
-
 
 @app.get("/section_info/{section_id}")
 def get_section_info(section_id: str):
@@ -22,24 +20,23 @@ def get_section_info(section_id: str):
 
     section_id = section_id.lower()
 
-    response = requests.get("https://whos-my-ta.fly.dev/section_id/" + section_id)
+    response = requests.get(MICROSERVICE_LINK + section_id)
 
-    # You can check out what the response body looks like in terminal using the print statement
+    if response.status_code != 200:
+        raise HTTPException(status_code=404, detail="Section not found")
+
     data = response.json()
-    print(data)
-    ta_name_list = data["ta_names"]
-    ta1_name = ta_name_list[0]["fname"] + " " + ta_name_list[0]["lname"]
-    ta2_name = ta_name_list[1]["fname"] + " " + ta_name_list[1]["lname"]
+    ta_name_list = data.get("ta_names", [])  # Get the TA names, or an empty list if not available
 
-    print(ta1_name)
-
-    # TODO
-    if section_id == "a":
-        return {
-            "section": "section_name",
-            "start_time": "HH:MM",
-            "end_time": "HH:MM",
-            "ta": ["taName1", "taName2"]
+    if section_id in RECITATION_HOURS:
+        # Construct the response JSON
+        response_data = {
+            "section": section_id,
+            "start_time": RECITATION_HOURS[section_id].split("~")[0],  
+            "end_time": RECITATION_HOURS[section_id].split("~")[1],    
+            "ta": [f"{ta['fname']} {ta['lname']}" for ta in ta_name_list], 
+            "recitation time": RECITATION_HOURS[section_id]
         }
+        return response_data
     else:
         raise HTTPException(status_code=404, detail="Invalid section id")
